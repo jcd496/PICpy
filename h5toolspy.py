2aec1fa9684c6bb18636de93e5e37e205965d8fe
John <john.donaghy@protonmail.com>
2020-11-18 22:23:47 -0500

    incorporate xarray for unit handling

--- h5toolspy.py
+++ h5toolspy.py
@@ -6,18 +6,18 @@ from bs4 import BeautifulSoup
 import os
 import h5py
 import numpy as np
+import xarray as xr
 import matplotlib
 import matplotlib.pyplot as plt
 
-def getH5Grid(root: str, file_: str, field: str, time: int, corner: tuple, dims: tuple):
+def getH5Grid(root: str, file_: str, field: str, time: int):
     """
     args:
         root [str]: path to root directory
         file_ [str]: xdmf file to open, usually tfd or tfd_moments
         field [str]: jx_ec, jy_ec, jz_ec, ex_ec, ey_ec, ez_ec, hx_fc, hy_fc, hz_fc
         time [int]: index into list of saved files of type file_
-        corner [tuple/int]: tuple of ints, lower left hand corner of grid to load
-        dims: [tuple/int]: dimensions of grid to load
+
     returns:
         slice of grid stored in file_ at specified time as a numpy array
     """
@@ -30,7 +30,18 @@ def getH5Grid(root: str, file_: str, field: str, time: int, corner: tuple, dims:
    
     with open(root + timeFileXdmf, 'r') as f1:
         soup1 = BeautifulSoup(f1, features='html.parser')
-        
+
+    dataItems = soup1.find_all('dataitem')
+    origin = dataItems[0]
+    
+    origin = np.array(origin.contents[0].strip().split()).astype('float')
+    DxDyDz = dataItems[1]
+    DxDyDz = np.array(DxDyDz.contents[0].strip().split()).astype('float')
+
+    topology = soup1.find_all('topology')[0]
+    topology = np.array(topology.attrs['dimensions'].split(" ")).astype('int')  - 1
+    
+
     attributeFields = soup1.find_all('attribute')
     
     for i in range(len(attributeFields)):
@@ -42,20 +53,32 @@ def getH5Grid(root: str, file_: str, field: str, time: int, corner: tuple, dims:
     dataFile = h5py.File(os.path.join(root, dataFile), 'r')
     key = keys.split('/')[1]
     
-    x, y, z = corner
-    dimx, dimy, dimz = dims
-    return np.array(dataFile[key][field]['p0']['3d'][z:z+dimz, y:y+dimy, x:x+dimx])
+    data = np.array(dataFile[key][field]['p0']['3d'])
+    stop = origin + DxDyDz*topology
+
+    zUnits = np.linspace(origin[0], stop[0], topology[0])
+    yUnits = np.linspace(origin[1], stop[1], topology[1])
+    xUnits = np.linspace(origin[2], stop[2], topology[2])
     
-def selectSubSpace(xp: int, yp: int, cellsPerPatch = 32, numPatches = 1):
+    grid = xr.DataArray(data, 
+                        coords = [zUnits, yUnits, xUnits],
+                        dims = ['z', 'y', 'x']
+                       )
+    
+    return grid
+    
+def selectSubSpace(xp: tuple, yp: tuple, color: str='g' ):
     """
     args:
-        yp [int]: Left edge of box
-        zp [int]: bottom edge of box
-        cellsPerPatch [int]: number of grid cells in a singled dimension of patch
-        numPatches [int]: number of patches to select in a single dimension
+        xp [tuple/int]: (xleft, xright)
+        yp [tuple/int]: (ybottom, ytop)
+        color [str]: matplotlib color 
     """
-    width = (cellsPerPatch-1)*numPatches
-    rect = matplotlib.patches.Rectangle((xp,yp), width, width, linewidth=1,edgecolor='g',facecolor='none')
+    xleft, xright = xp
+    ybottom, ytop = yp
+    width = xright - xleft
+    height = ytop - ybottom
+    rect = matplotlib.patches.Rectangle((xleft,ybottom), width, height, linewidth=1,edgecolor=color,facecolor='none')
     plt.gca().add_patch(rect)
     return 
 
