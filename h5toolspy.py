0f12d55e42c0b2fca8727a355a103f4fab401cea
John <john.donaghy@protonmail.com>
2020-11-21 17:17:11 -0500

    classify h5 processor to allow interfacing fields and particles

--- h5toolspy.py
+++ h5toolspy.py
@@ -7,113 +7,188 @@ import os
 import h5py
 import numpy as np
 import xarray as xr
+from math import ceil
 import matplotlib
 import matplotlib.pyplot as plt
 
-def getH5Grid(root: str,
-              file_: str,
-              field: str,
-              time: int,
-              xmin: float = None,
-              xmax: float = None,
-              ymin: float = None,
-              ymax: float = None,
-              zmin: float = None,
-              zmax: float = None):
-    """
-    args:
-        root [str]: path to root directory
-        file_ [str]: xdmf file to open, usually tfd or tfd_moments
-        field [str]: jx_ec, jy_ec, jz_ec, ex_ec, ey_ec, ez_ec, hx_fc, hy_fc, hz_fc
-        time [int]: index into list of saved files of type file_
-        (x,y,z)(min,max) [float]: coordinates to slice fields. lazy execution loading.  min==max for 2D slice
+class H5Processor:
+    def __init__(self, root: str):
+        self.root = root
+        self.time = 0
+        self.xUnits, self.yUnits, self.zUnits = self.__buildGrid()
+        self.slice = None
+    def __nearest(self, array, value, op):
+            """
+            return index of nearest value 
+            """
+            if value is not None:
+                return np.abs(array - value).argmin()
+            else:
+                return 0 if op == 'min' else len(array)
+        
+    def __buildGrid(self):
+        
+        
+        with open(os.path.join(self.root, 'tfd' +'.xdmf'),  'r') as f:
+            soup = BeautifulSoup(f, features='html.parser')
 
-    returns:
-        slice of grid stored in file_ at specified time as a xarray
-    """
-    with open(os.path.join(root, file_ +'.xdmf'),  'r') as f:
-        soup = BeautifulSoup(f, features='html.parser')
+        timeFilesXdmf = soup.grid.find_all('xi:include')
+        timeFileXdmf = timeFilesXdmf[self.time]['href']
 
-    timeFilesXdmf = soup.grid.find_all('xi:include')
-    timeFileXdmf = timeFilesXdmf[time]['href']
-    print(f'Loading {field} from File: {timeFileXdmf}')
-   
-    with open(root + timeFileXdmf, 'r') as f1:
-        soup1 = BeautifulSoup(f1, features='html.parser')
+        with open(self.root + timeFileXdmf, 'r') as f1:
+            soup1 = BeautifulSoup(f1, features='html.parser')
 
-    dataItems = soup1.find_all('dataitem')
-    origin = dataItems[0]
+        dataItems = soup1.find_all('dataitem')
+        origin = dataItems[0]
+
+        origin = np.array(origin.contents[0].strip().split()).astype('float')
+        DxDyDz = dataItems[1]
+        DxDyDz = np.array(DxDyDz.contents[0].strip().split()).astype('float')
+
+        topology = soup1.find_all('topology')[0]
+        topology = np.array(topology.attrs['dimensions'].split(" ")).astype('int') - 1
+        
+        stop = origin + DxDyDz*topology 
+        
+        zUnits = np.linspace(origin[0], stop[0], topology[0])
+        yUnits = np.linspace(origin[1], stop[1], topology[1])
+        xUnits = np.linspace(origin[2], stop[2], topology[2])
+
+        return xUnits, yUnits, zUnits
+        
     
-    origin = np.array(origin.contents[0].strip().split()).astype('float')
-    DxDyDz = dataItems[1]
-    DxDyDz = np.array(DxDyDz.contents[0].strip().split()).astype('float')
+    def getH5Grid(self,
+                  field: str,
+                  time: int,
+                  xmin: float = None,
+                  xmax: float = None,
+                  ymin: float = None,
+                  ymax: float = None,
+                  zmin: float = None,
+                  zmax: float = None):
+        """
+        args:
+            field [str]: jx_ec, jy_ec, jz_ec, ex_ec, ey_ec, ez_ec, hx_fc, hy_fc, hz_fc
+            time [int]: index into list of saved files of type file_
+            (x,y,z)(min,max) [float]: coordinates to slice fields. lazy execution loading.  min==max for 2D slice
 
-    topology = soup1.find_all('topology')[0]
-    topology = np.array(topology.attrs['dimensions'].split(" ")).astype('int') - 1
+        returns:
+            slice of grid stored from specified time as a xarray
+        """
+        
+        file_ = 'tfd' if field in ['jx_ec', 'jy_ec', 'jz_ec', 
+                                   'ex_ec', 'ey_ec', 'ez_ec', 
+                                   'hx_fc', 'hy_fc', 'hz_fc'] else 'tfd_moments'
+
+        if time != self.time:
+            self.time = time
+        
+        with open(os.path.join(self.root, file_ +'.xdmf'),  'r') as f:
+            soup = BeautifulSoup(f, features='html.parser')
+
+        timeFilesXdmf = soup.grid.find_all('xi:include')
+        timeFileXdmf = timeFilesXdmf[self.time]['href']
+
+        with open(self.root + timeFileXdmf, 'r') as f1:
+            soup1 = BeautifulSoup(f1, features='html.parser')
+
+        attributeFields = soup1.find_all('attribute')
+
+        for i in range(len(attributeFields)):
+            if attributeFields[i].attrs['name'] == field:
+                dataFile, keys = attributeFields[i].dataitem.get_text().split()[0].split(':')
+                break
+
+
+        dataFile = h5py.File(os.path.join(self.root, dataFile), 'r')
+        key = keys.split('/')[1]
+
+        xmin = self.__nearest(self.xUnits, xmin, 'min')
+        xmax = self.__nearest(self.xUnits, xmax, 'max')
+        ymin = self.__nearest(self.yUnits, ymin, 'min')
+        ymax = self.__nearest(self.yUnits, ymax, 'max')
+        zmin = self.__nearest(self.zUnits, zmin, 'min')
+        zmax = self.__nearest(self.zUnits, zmax, 'max')
+
+        if zmax == zmin: 
+            zmax = zmin + 1
+            self.slice = zmin
+        elif ymax == ymin:
+            ymax = ymin + 1
+            self.slice = ymin
+        elif xmax == xmin:
+            xmax = xmin + 1
+            self.slice = xmin
+
+        zUnits = self.zUnits[zmin:zmax]
+        yUnits = self.yUnits[ymin:ymax]
+        xUnits = self.xUnits[xmin:xmax]
+
+        data = dataFile[key][field]['p0']['3d'][zmin:zmax, ymin:ymax, xmin:xmax]
+
+        grid = xr.DataArray(data, 
+                            coords = [zUnits, yUnits, xUnits],
+                            dims = ['z', 'y', 'x']
+                           )
+        return grid  
     
-    attributeFields = soup1.find_all('attribute')
-    
-    for i in range(len(attributeFields)):
-        if attributeFields[i].attrs['name'] == field:
-            break
+    def selectSubSpace(self, xp: tuple, yp: tuple, axes: tuple=('y','z'), color: str='g', cellsPerPatch: int=32):
+        """
+        Takes requested subspace limits and converts to integer number of patches most
+        nearly representative of requested subspace. Plots selected patches.
+        Only supports square subspaces
+        used to collect particles from checkpoint file, which are collected patchwise 
+        args:
+            xp [tuple/int]: (xleft, xright)
+            yp [tuple/int]: (ybottom, ytop)
+            axis [tuple/str]: (xaxis dimension, yaxis dimension)
+            color [str]: matplotlib color
+        returns:
+            ((int,int,int), int): lower left corner of selected space, number of patches per dim
+        """
+        xleft, xright = xp
+        ybottom, ytop = yp
+
+        dimx, dimy = axes
+
+        if (dimx or dimy) not in ['x', 'y', 'z']:
+            raise Exception('Improper spatial coordinates for plot axis')
+        slice_ = ['x','y','z']
+        slice_.remove(dimx)
+        slice_.remove(dimy)
+        slicedim = slice_[0]
+        
+        #convert requested coordinates to cell indexes
+        xunits = getattr(self, dimx + 'Units')
+        yunits = getattr(self, dimy + 'Units')
+        sliceUnits = getattr(self, slicedim + 'Units')
+        
+        ixl = self.__nearest(xunits, xleft, 'min')
+        ixr = self.__nearest(xunits, xright, 'max')
+        iyb = self.__nearest(yunits, ybottom, 'min')
+        iyt = self.__nearest(yunits, ytop, 'max')
+
+        islice = self.__nearest(sliceUnits, self.slice, 'min')
+        #calculate number of patches requested
+        span = (ixr - ixl) 
+        nPatch = ceil(span/cellsPerPatch)
+        #patch alignment and covnert back to coordinates
+        alignx, aligny = ixl//cellsPerPatch * cellsPerPatch, iyb//cellsPerPatch * cellsPerPatch
+        alignSlice = islice//cellsPerPatch * cellsPerPatch
+        lLeftX = xunits[alignx]
+        lLeftY = yunits[aligny]
+        
+        idxMap = {'x':0, 'y':1, 'z':2}
+        if dimx == 'y' and dimy == 'z':
+            cornerPatchIdx = (alignSlice, alignx, aligny)
+        elif dimx == 'y' and dimy == 'x':
+            cornerPatchIdx = (aligny, alignx, alignSlice)
             
-    dataFile, keys = attributeFields[i].dataitem.get_text().split()[0].split(':')
- 
-    dataFile = h5py.File(os.path.join(root, dataFile), 'r')
-    key = keys.split('/')[1]
-    
-    stop = origin + DxDyDz*topology 
+        cornerCoord = (lLeftX, lLeftY)
+        
+        width = nPatch * cellsPerPatch
+        height = nPatch * cellsPerPatch
 
-    zUnits = np.linspace(origin[0], stop[0], topology[0])
-    yUnits = np.linspace(origin[1], stop[1], topology[1])
-    xUnits = np.linspace(origin[2], stop[2], topology[2])
-    
-    def nearest(array, value, op):
-        """
-        return index of nearest value 
-        """
-        if value is not None:
-            return np.abs(array - value).argmin()
-        else:
-            return 0 if op == 'min' else len(array)
-    
-    
-    xmin = nearest(xUnits, xmin, 'min')
-    xmax = nearest(xUnits, xmax, 'max')
-    ymin = nearest(yUnits, ymin, 'min')
-    ymax = nearest(yUnits, ymax, 'max')
-    zmin = nearest(zUnits, zmin, 'min')
-    zmax = nearest(zUnits, zmax, 'max')
-    
-    if zmax == zmin: zmax = zmin + 1
-    if ymax == ymin: ymax = ymin + 1
-    if xmax == xmin: xmax = xmin + 1
-
-    zUnits = zUnits[zmin:zmax]
-    yUnits = yUnits[ymin:ymax]
-    xUnits = xUnits[xmin:xmax]
-    
-    data = dataFile[key][field]['p0']['3d'][zmin:zmax, ymin:ymax, xmin:xmax]
-
-    grid = xr.DataArray(data, 
-                        coords = [zUnits, yUnits, xUnits],
-                        dims = ['z', 'y', 'x']
-                       )
-    return grid  
-    
-def selectSubSpace(xp: tuple, yp: tuple, color: str='g' ):
-    """
-    args:
-        xp [tuple/int]: (xleft, xright)
-        yp [tuple/int]: (ybottom, ytop)
-        color [str]: matplotlib color 
-    """
-    xleft, xright = xp
-    ybottom, ytop = yp
-    width = xright - xleft
-    height = ytop - ybottom
-    rect = matplotlib.patches.Rectangle((xleft,ybottom), width, height, linewidth=1,edgecolor=color,facecolor='none')
-    plt.gca().add_patch(rect)
-    return 
-
-
+        rect = matplotlib.patches.Rectangle(cornerCoord, width, height, linewidth=1,edgecolor=color,facecolor='none')
+        plt.gca().add_patch(rect)
+        return cornerPatchIdx, nPatch
